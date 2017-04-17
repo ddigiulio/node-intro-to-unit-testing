3d7c322216e2668a17784292988ddf45b908c887
ddigiulio <ddigiulio90@gmail.com>
2017-04-16 21:37:49 -0700

    added fizzbuzz test

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,33 @@
+const should = require('chai').should();
+const fizzBuzzer = require('../fizzBuzzer');
+
+describe('fizzBuzzer', function(){
+
+    it('should submit fizz, buzz, or fizzbuzz on 3, 5, and 15, depending on value', function (){
+
+        const normalCases = [
+            { a: 3, expected: "fizz"},
+            { a: 5, expected: "buzz"},
+            { a: 15, expected: "fizzbuzz"}
+        ];
+
+        normalCases.forEach(function(input){
+            const answer = fizzBuzzer(input.a);
+            answer.should.equal(input.expected);           
+        });
+
+    });
+
+    it('should raise errors if args are not numbers', function(){
+
+        const badInputs = ['a', "beans", "fizz buzz", "fifteen"];
+        
+        badInputs.forEach(function(input){
+            (function (){
+                fizzBuzzer(input);
+            }).should.throw(Error);
+        });
+
+    });
+
+});
